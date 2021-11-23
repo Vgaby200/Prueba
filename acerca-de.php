1160934e5a15fe78643d27fc51dc65010e799750
Gabriela <93696987+Vgaby200@users.noreply.github.com>
2021-11-23 17:05:46 -0600

    Add files via upload

---
+++ acerca-de.php
@@ -0,0 +1,30 @@
+<br>
+      <br>
+      <main class="main">
+      <section class="sombra">
+        <h3 class="seccion-acercaDe__titulo">Conoce algo de Nosotros</h3>
+        <h4 class="acercaDe__titulo">HISTORIA</h4>
+        <p class="acercaDe__texto">En MindSoft tenemos la convicción de cualquier persona puede 
+            lograr grandes cosas si se tiene la determinación para hacerle frente a los 
+            retos que se presenten, así como este gran proyecto.
+            </p>
+
+            <br>
+            <h4 class="acercaDe__titulo">FUNDADORES</h4>
+        <p class="acercaDe__texto">Somos un equipo de alumnos matriculados en Instituto 
+            Tecnológico de la Laguna, en la ciudad de torreón Coahuila, estudiantes de la carrera de ingeniería 
+            en sistemas computaciones. Actualmente tenemos entre 20 y 21 años y este proyecto que surgió con el fin de que cada 
+            cliente tenga el producto que desee, pero sobre todo una experiencia inigualable a la hora de hacer 
+            su compra por internet.
+        </p>
+
+            <br>
+            <h4 class="acercaDe__titulo">PRINCIPIOS DE LA TIENDA</h4>
+        <p class="acercaDe__texto">En MindSoft buscamos seguir los principios éticos y morales que 
+            hemos establecido desde que somos un pequeño negocio hasta hoy en día que 
+            somos una empresa globalmente localizada </p>
+    </section>
+
+       <br>
+       <br>
+    </main>
